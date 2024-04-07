c01bdc044f5e17e70752af3380784d0ecb816304
IvarNilsson <ivar.j.nilsson@gmail.com>
2024-04-07 10:29:19 +0200

    improved send over uart

--- script/send_over_uart.sh
+++ script/send_over_uart.sh
@@ -1,38 +1,28 @@
-#!/bin/sh
+stty -F /dev/ttyUSB0 115200
 
-# Read the file line by line
+#stty -F /dev/ttyUSB0 crtscts    # To enable RTS/CTS flow control
+#stty -F /dev/ttyUSB0 -crtscts   # To disable flow control with
+#stty -a -F /dev/ttyUSB0         # To view the current settings for UART3, use
+
+# Check if the file is provided as argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <filename>"
+    exit 1
+fi
+
+filename=$1
+
+# Read each line from the file and convert to hexadecimal
 while IFS= read -r line; do
-    # Initialize variables to hold each bit
-    c0=""
-    c1=""
-    c2=""
-    c3=""
-    c4=""
-    c5=""
-    c6=""
-    c7=""
-    i=0
 
-    # Iterate over each character in the line
-    for char in $(echo "$line" | sed 's/./& /g'); do
-        # Assign each character to the corresponding variable
-        case $i in
-        0) c0="$char" ;;
-        1) c1="$char" ;;
-        2) c2="$char" ;;
-        3) c3="$char" ;;
-        4) c4="$char" ;;
-        5) c5="$char" ;;
-        6) c6="$char" ;;
-        7) c7="$char" ;;
-        esac
+    hex=$(echo "obase=16;ibase=2;$line" | bc)
+    # Add leading zeros if necessary note: dont know if nessisary
+    if [ ${#hex} -lt 2 ]; then
+        hex="0$hex"
+    fi
 
-        # Increment counter
-        i=$((i + 1))
-    done
+    echo "$hex"
+    echo -n "\x$hex" >/dev/ttyUSB0
+done <"$filename"
 
-    # Echo the next 8 bits together
-    echo "$c0 $c1 $c2 $c3 $c4 $c5 $c6 $c7"
-    echo -n "\0$c0\0$c1\0$c2\0$c3\0$c4\0$c5\0$c6\0$c7" >/dev/ttyUSB0
-
-done <src/instruction_mem_temp.mem
+#echo -n "\xFF" > /dev/ttyUSB0
