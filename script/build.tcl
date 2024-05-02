172dc9e466e8a539581e3f2dec717ee8d7612e17
IvarNilsson <ivar.j.nillsson@gmail.com>
2024-05-02 09:50:26 +0200

    small fix

--- script/build.tcl
+++ script/build.tcl
@@ -14,7 +14,6 @@ set top_module [file join "$ROOT" src top.sv]
 
 # HDL files
 add_files -norecurse [file join "$ROOT" src top.sv]
-add_files -norecurse [file join "$ROOT" src lib common_instructions_pkg.sv]
 add_files -norecurse [file join "$ROOT" src lib common_pkg.sv]
 add_files -norecurse [file join "$ROOT" src alu.sv]
 add_files -norecurse [file join "$ROOT" src control.sv]
